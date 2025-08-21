3d2e0717a1d9e6c6b0ac4a0d7a6bc38c71b5cf66
Gabriel Farro <farrogabriel90@gmail.com>
2025-08-21 00:08:25 -0500

    Desarrollo: Se agregó controlador Usuario

--- WebGitPrueba/obj/Debug/net8.0/WebGitPrueba.AssemblyInfo.cs
+++ WebGitPrueba/obj/Debug/net8.0/WebGitPrueba.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebGitPrueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb331c8ca594c36d153a3dc7e35c35d85029744a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebGitPrueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebGitPrueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
