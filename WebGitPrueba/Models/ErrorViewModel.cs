bb331c8ca594c36d153a3dc7e35c35d85029744a
Gabriel Farro <farrogabriel90@gmail.com>
2025-08-21 00:05:00 -0500

    Main: Proyecto base

---
+++ WebGitPrueba/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebGitPrueba.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
